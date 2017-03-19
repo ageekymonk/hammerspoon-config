db4f3d8b67d097452dcf5420041a9a909d686831
ramz <ramzthecoder@gmail.com>
2017-03-20 08:39:05 +1100

    Adding lock screen module

---
+++ modules/lock.lua
@@ -0,0 +1,16 @@
+local hotkey = require 'hs.hotkey'
+
+local function module_init()
+  lock_keys = lock_keys or {{"cmd", "ctrl", "alt"}, "l"}
+  local mash = config:get("lock.mash", lock_keys[1])
+  local key = config:get("lock.key", lock_keys[2])
+
+  hotkey.bind(mash, key, function()
+                os.execute("/System/Library/CoreServices/Menu\\ Extras/User.menu/Contents/Resources/CGSession -suspend")
+  end)
+
+end
+
+return {
+  init = module_init
+}
