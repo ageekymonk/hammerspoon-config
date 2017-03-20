e65f8403dc4a14c18ea733f97d97d358f3460f67
ramz <ramzthecoder@gmail.com>
2017-03-20 11:44:34 +1100

    Setting the right indentation

--- config.lua
+++ config.lua
@@ -1,70 +1,71 @@
 local config = {}
 
 config.modules = {
-    "arrangement",
-    "monitors",
-    "repl",
-    "reload",
-    "arrows",
-    "lock",
-    "fullscreen",
-    "winmgmt"
+  "arrangement",
+  "hop",
+  "monitors",
+  "console",
+  "reload",
+  "arrows",
+  "lock",
+  "fullscreen",
+  "winmgmt"
 }
 
 -- Maps monitor id -> screen index.
 config.monitors = {
-    autodiscover = true,
-    rows = 1
+  autodiscover = true,
+  rows = 1
 }
 
 -- Window arrangements.
 config.arrangements = {
-    fuzzy_search = {
-        mash = {"cmd", "ctrl", "alt"},
-        key = "Z"
-    },
-    {
-        name = "zen",
-        alert = true,
-        mash = { "cmd", "ctrl", "alt" },
-        key = "A",
-        arrangement = {
-            {
-                app_title = "^Mail",
-                monitor = 1,
-                position = "full_screen",
-            },
-            {
-                app_title = "^Slack",
-                monitor = 4,
-                position = "left"
-            },
-            {
-                app_title = "^Messages",
-                monitor = 4,
-                position = function(d)
-                    return d:translate_from('bottom_right', {
-                        y = 42,
-                        h = -40
-                    })
-                end
-            },
-            {
-                app_title = "^ChronoMate",
-                monitor = 4,
-                position = function(d)
-                    return d:translate_from('top_right', {
-                        h = 42
-                    })
-                end
-            },
-            {
-                app_title = "^Spotify",
-                monitor = 6,
-                position = "full_screen",
-            }
-        }
+  fuzzy_search = {
+    mash = {"cmd", "ctrl", "alt"},
+    key = "Z"
+  },
+  {
+    name = "zen",
+    alert = true,
+    mash = { "cmd", "ctrl", "alt" },
+    key = "A",
+    arrangement = {
+      {
+        app_title = "^Mail",
+        monitor = 1,
+        position = "full_screen",
+      },
+      {
+        app_title = "^Slack",
+        monitor = 4,
+        position = "left"
+      },
+      {
+        app_title = "^Messages",
+        monitor = 4,
+        position = function(d)
+          return d:translate_from('bottom_right', {
+                                    y = 42,
+                                    h = -40
+          })
+        end
+      },
+      {
+        app_title = "^ChronoMate",
+        monitor = 4,
+        position = function(d)
+          return d:translate_from('top_right', {
+                                    h = 42
+          })
+        end
+      },
+      {
+        app_title = "^Spotify",
+        monitor = 6,
+        position = "full_screen",
+      }
     }
+  }
 }
 
 return config
